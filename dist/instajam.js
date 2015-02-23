194afb067b5736beb7787c42951bd79229056a5a
Mike Fowler <mike.fowler@airbnb.com>
2015-02-23 10:22:22 -0800

    Update build products

--- dist/instajam.js
+++ dist/instajam.js
@@ -1,6 +1,6 @@
-/*! Instajam - v2.0.0 - 2013-11-03
+/*! Instajam - v2.0.0 - 2015-02-23
 * http://github.com/mikefowler/instajam/
-* Copyright (c) 2013 Mike Fowler; Licensed MIT */
+* Copyright (c) 2015 Mike Fowler; Licensed MIT */
 (function(Instajam) {
 
   'use strict';
@@ -18,7 +18,7 @@
       throw new InstajamError("Client ID and Redirect URI are required.");
     }
 
-    // If the app is requesting additional 
+    // If the app is requesting additional p
     // scopes, build a string to append to
     // the auth URL.
     if (options.scope && typeof options.scope === 'object') {
@@ -216,11 +216,19 @@
       // Make a request to the API
       User.prototype.search.call(this, id, {}, function(result) {
 
-        // If the initial user search yields any
-        // results, then just return the first, but
+        // Go through the results and check for
+        // a perfect match, then query again with that user's ID
         // otherwise return nothing.
-        if (result.data && result.data.length === 1) {
-          result = result.data[0];
+        if (result.data) {    
+            var userIds = result.data;
+            result = false;
+
+            for (var i = 0; i < userIds.length; i++) {
+                if (userIds[i].username === id) {
+                    User.prototype.get(parseInt(userIds[i].id, 10), callback);
+                    return;
+                }
+            }
         } else {
           result = false;
         }
